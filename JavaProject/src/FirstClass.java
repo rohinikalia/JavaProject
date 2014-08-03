8902ba1f1676da410434fcecd3e423065d75b460
Rohini <Rohini@IBM881-PK0T8N1>
2014-08-03 09:37:46 +0100

    Add a simple output change

--- JavaProject/src/FirstClass.java
+++ JavaProject/src/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 	public static void doMethod()
 	{
 		System.out.println("My first java class");
+		System.out.println("My first Repository");
 	}
 	public static void main(String[] args)
 	{
