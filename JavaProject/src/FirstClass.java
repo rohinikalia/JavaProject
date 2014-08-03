eb9a1a79422e1dbc45b2ba3255f0278ab2c9b4d9
Rohini <Rohini@IBM881-PK0T8N1>
2014-08-03 09:19:34 +0100

    Initial Checking of java project.

---
+++ JavaProject/src/FirstClass.java
@@ -0,0 +1,13 @@
+
+public class FirstClass {
+
+	public static void doMethod()
+	{
+		System.out.println("My first java class");
+	}
+	public static void main(String[] args)
+	{
+		doMethod();
+		
+	}
+}
